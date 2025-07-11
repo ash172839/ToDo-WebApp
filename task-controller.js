8ce1c4ca3fc944c2abc01e260f9a97a12be122d8
ash172839 <119349590+ash172839@users.noreply.github.com>
2025-07-12 00:29:37 +0530

    Add files via upload

--- task-controller.js
+++ task-controller.js
@@ -1,10 +1,15 @@
 import { Task_Operations } from '../data/services/task-operations.js';
-window.addEventListener('load',bindEvents)
-export function bindEvents() {
+window.addEventListener('load',init)
+function bindEvents() {
     document.getElementById("add").addEventListener('click',addTask);
 }
 
-export function addTask(){
+function init(){
+    bindEvents();
+    showCount();
+    disableButtons();
+}
+function addTask(){
     const fields=['id','name','desc','date','time','url'];
     const taskObject={};
     for(let field of fields){
@@ -12,5 +17,44 @@ export function addTask(){
         taskObject[field]=fieldValue;
     }
     Task_Operations.add(taskObject);
+    printTask(taskObject);
+    showCount();
+}
+function createIcon(className,fn){
+    const iconTag=document.createElement('i');
+    iconTag.className=`fa-solid ${className} me-2 hand`;
+    iconTag.addEventListener('click',fn);
+    return iconTag;
+}
+
+function disableButtons(){
+    document.querySelector('#remove').setAttribute("disabled",true);
+    document.querySelector('#update').setAttribute("disabled",true);
+}
+
+function toggleDelete(){
+    console.log("toggle delete");
     
+}
+
+function edit(){
+
+}
+
+function showCount(){
+    document.querySelector('#total').innerText=Task_Operations.getSize();
+    document.querySelector('#mark').innerText=Task_Operations.getMark();
+    document.querySelector('#unmark').innerText=Task_Operations.getUnMark();
+}
+
+function printTask(taskObject){
+    const tbody=document.querySelector('#tasks');
+    const tr=tbody.insertRow();
+    for(let key in taskObject){
+        let td=tr.insertCell();
+        td.innerText=taskObject[key];
+    }
+    let td=tr.insertCell();
+    td.appendChild(createIcon('fa-pen',edit));
+    td.appendChild(createIcon('fa-trash',toggleDelete));
 }
--- task-operations.js
+++ task-operations.js
@@ -1,5 +1,17 @@
 import Task from '../models/task.js';
 export const Task_Operations={
+    getSize(){
+        return this.tasks.length;
+    },
+
+    getMark(){
+        return 0;
+    },
+
+    getUnMark(){
+        return 0;
+    },
+    
     tasks:[],
     add(taskObject){
         let task=new Task();
